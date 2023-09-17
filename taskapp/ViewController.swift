6699f661a39dcee85ba90bac17d1a117d93b9265
okami-ap <y.okami@alphapolis.co.jp>
2023-09-17 18:59:21 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by WEBSYSTEM-MAC29 on 2023/09/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
